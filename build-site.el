f6ee93f1cafc4cea22695c84a354db23d6e1fb58
Ishak <ishak.ariif@gmail.com>
2024-12-29 02:54:29 +0000

    Initial commit

---
+++ build-site.el
@@ -0,0 +1,25 @@
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Get rid of the validate link at the bottom
+(setq org-html-validation-link nil)
+
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "org-site:main"
+             :recursive t
+             :base-directory "./content"
+             :publishing-function 'org-html-publish-to-html
+             :publishing-directory "./public"
+             :with-author nil           ;; Don't include author name
+             :with-creator nil          ;; Don't include Emacs and Org versions in footer
+             :with-toc nil              ;; Don't include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :time-stamp-file nil)))    ;; Don't include time stamp in file
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete!")
